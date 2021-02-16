678d0ef34f2c0db85403a42decbba5214392bb5b
Colter Ulrich <cju5025@gmail.com>
2021-02-16 11:37:23 -0700

    Reseeds audioFiles

--- seeds/01-add-audioFiles.js
+++ seeds/01-add-audioFiles.js
@@ -6,29 +6,125 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('audioFiles').insert([
         {
-          name: 'cassette 01',
-          location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/1612906908118_Cymatics+-+Eternity+Gear+Foley+1+-+Cassette+Deck+Button.wav',
+          name: 'Alien Ambience 01',
+          location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/Alien_Ambiance_01.wav',
           category: 'Film & Game',
-          subCategory: 'Foley',
-          user_id: 15,
+          subCategory: 'Machine',
+          user_id: 20,
           price: 1
       },
       {
-        name: 'cassette 02',
-        location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/1612907351623_Cymatics+-+Eternity+Gear+Foley+2+-+Cassette+Deck+Button.wav',
+        name: 'Alien Ambience 02',
+        location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/Alien_Ambiance_02.wav',
         category: 'Film & Game',
-        subCategory: 'Foley',
-        user_id: 15,
+        subCategory: 'Machine',
+        user_id: 20,
         price: 1
     },
     {
-      name: 'cassette 03',
-      location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/1612907359498_Cymatics+-+Eternity+Gear+Foley+3+-+Cassette+Deck+Button.wav',
+      name: 'Alien Ambience 03',
+      location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/Alien_Ambience_02.wav',
       category: 'Film & Game',
-      subCategory: 'Foley',
-      user_id: 14,
+      subCategory: 'Machine',
+      user_id: 20,
       price: 1
   },
+  {
+    name: 'Alien Ambience 04',
+    location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/Alien_Ambience_03.wav',
+    category: 'Film & Game',
+    subCategory: 'Machine',
+    user_id: 20,
+    price: 1
+},
+{
+  name: 'Alien Texture 01',
+  location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/Alien_Texture_01.wav',
+  category: 'Film & Game',
+  subCategory: 'Machine',
+  user_id: 21,
+  price: 1
+},
+{
+  name: 'Alien Texture 01',
+  location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/Alien_Texture_01.wav',
+  category: 'Film & Game',
+  subCategory: 'Machine',
+  user_id: 21,
+  price: 1
+},
+{
+  name: 'Alien Texture 02',
+  location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/Alien_Texture_02.wav',
+  category: 'Film & Game',
+  subCategory: 'Machine',
+  user_id: 21,
+  price: 1
+},
+{
+  name: 'Alien Voices 01',
+  location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/Alien_Voices_01.wav',
+  category: 'Film & Game',
+  subCategory: 'Machine',
+  user_id: 21,
+  price: 1
+},
+{
+  name: 'Dark Textures 01',
+  location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/Dark_Textures_01.wav',
+  category: 'Film & Game',
+  subCategory: 'Machine',
+  user_id: 20,
+  price: 1
+},
+{
+  name: 'QE Dark Texture 01',
+  location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/QE_Dark_Texture_01.wav',
+  category: 'Film & Game',
+  subCategory: 'Machine',
+  user_id: 21,
+  price: 1
+},
+{
+  name: 'QE Long Impact 01',
+  location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/QE_Impact_long_01.wav',
+  category: 'Film & Game',
+  subCategory: 'Machine',
+  user_id: 21,
+  price: 1
+},
+{
+  name: 'QE Long Impact 02',
+  location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/QE_Impact_long_02.wav',
+  category: 'Film & Game',
+  subCategory: 'Machine',
+  user_id: 21,
+  price: 1
+},
+{
+  name: 'QE Long Impact 03',
+  location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/QE_Impact_long_03.wav',
+  category: 'Film & Game',
+  subCategory: 'Machine',
+  user_id: 21,
+  price: 1
+},
+{
+  name: 'QE Short Impact 01',
+  location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/QE_Impact_short_01.wav',
+  category: 'Machine',
+  subCategory: 'Film & Game',
+  user_id: 21,
+  price: 1
+},
+{
+  name: 'QE Short Impact 02',
+  location: 'https://audatixaudio.s3.us-east-2.amazonaws.com/wavs/QE_Impact_short_02.wav',
+  category: 'Machine',
+  subCategory: 'Film & Game',
+  user_id: 21,
+  price: 1
+},
       ]);
     });
 };
